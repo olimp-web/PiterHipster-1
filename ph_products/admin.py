00604656c388e1aa7a8c41399ac934d15babce2b
A. Kotelnikov <kotelnikov@enhancelab.ru>
2019-11-08 18:35:05 +0300

    base admin for catalog

--- ph_products/admin.py
+++ ph_products/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from .models import *
 # Register your models here.
+
+
+class ModificationInline(admin.TabularInline):
+    model = ProductModification
+    fields = ('color', 'size')
+
+
+class PhotoInline(admin.TabularInline):
+    model = ProductPhoto
+    fields = ('image', 'alt', 'is_main')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "description", "category", "price", "left_in_stock", "prime_cost")
+    inlines = [PhotoInline, ModificationInline]
+
+
+@admin.register(Category)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ("id", "title")
